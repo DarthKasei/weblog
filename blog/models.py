3b9e4c41d3f04086f2520a307f137a61111618e6
gcampbell01 <george.campbell@vmsuk.com>
2015-10-07 14:01:37 +0100

    fixes

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,20 @@
 from django.db import models
-from django.contrib import admin
+from django.utils import timezone
 
 # Create your models here.
+
 class Post(models.Model):
-	title = models.CharField(max_length=60)
-	body = models.TextField()
-	created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
 
-	def __unicode__(self):
-		return self.title
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
-class PostAdmin(admin.ModelAdmin):
-	search_fields = ["title"]
-
-admin.site.register(Post, PostAdmin)
+    def __str__(self):
+        return self.title
