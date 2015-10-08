86347f0f3d4c49d7c18180007c44237d93619b64
gcampbell01 <george.campbell@vmsuk.com>
2015-10-08 11:00:29 +0100

    fixes

--- blog/views.py
+++ blog/views.py
@@ -48,6 +48,7 @@ def post_publish(request, pk):
     post.publish()
     return redirect('blog.views.post_detail', pk=pk)
 
-#def publish(self):
-#    self.published_date = timezone.now()
-#    self.save()
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog.views.post_list')
